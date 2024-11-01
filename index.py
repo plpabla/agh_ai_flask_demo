cedbc39bbf37d9ca4c5f389a60f00dc2b1f1fe47
pawel-laptop <p.blaszczy@gmail.com>
2024-11-01 18:59:38 +0100

    add docstrings to achieve 10.00 score in pylint checks

--- index.py
+++ index.py
@@ -1,3 +1,7 @@
+"""Main module - contains routing for the Flask app.
+Also placed here the calculation logic as it is a simple demo app.
+"""
+
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -5,11 +9,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    """Simple hello world route."""
     return "Hello, World!"
 
 
 @app.route("/calculate")
 def calculate():
+    """Calculates the result of the operation on two arguments.
+    Possible operations: sum, diff, mul, div.
+
+    Argument:
+    operation -- operation to perform
+    arg1 -- first argument
+    arg2 -- second argument
+
+    Returns:
+    result of the operation on the two arguments
+    """
     operation = request.args.get("operation")
     arg1 = float(request.args.get("arg1", 0))
     arg2 = float(request.args.get("arg2", 0))
